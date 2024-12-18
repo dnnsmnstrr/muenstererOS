77776f8d8a48f7b79da61ec4a3192237f8a5f4c1
Dennis Muensterer <dennismuensterer@gmail.com>
2024-12-18 23:46:30 +0100

    Add other playlists

--- src/routes/playlists/+page.svelte
+++ src/routes/playlists/+page.svelte
@@ -3,10 +3,11 @@
 	import { capitalize } from '$lib/helper';
 	interface PlaylistItem {
 		title: string;
-		season: 'winter' | 'spring' | 'summer' | 'autumn';
-		year: number;
+		season?: 'winter' | 'spring' | 'summer' | 'autumn';
+		year?: number;
 		uri: string;
 		emoji?: string;
+		description?: string;
 		gif?: string;
 		imageId?: string;
 	}
@@ -55,7 +56,8 @@
 			year: 2023,
 			season: 'autumn',
 			uri: '2BamYUcYazuRqudq38MX8Q',
-			imageId: 'ab67706c0000da8482bdfe8492e17ebe3277359f'
+			imageId: 'ab67706c0000da8482bdfe8492e17ebe3277359f',
+			gif: 'https://media4.giphy.com/media/G3lxvBMhGu53y/giphy.webp'
 		},
 		{
 			title: 'summer pt. ii',
@@ -75,6 +77,98 @@
 	];
 
 	const currentPlaylist = playlists[0]
+	const otherPlaylists: Record<string, PlaylistItem> = {
+		"spring": {
+			uri: "68z163CKUImi0QB6ZpSigr",
+			title: 'Spring',
+			description: 'All songs from my spring playlists',
+			emoji: '🌼',
+		},
+		"summer": {
+			uri: "5wpb3PdQOzFb3jurTcVfZO",
+			title: 'Summer',
+			description: 'All songs from my summer playlists',
+			emoji: '☀️',
+		},
+		"autumn": {
+			uri: "22XncHS02W5RWJ1yM4lV6l",
+			title: 'Autumn',
+			description: 'All songs from my autumn playlists',
+			emoji: '🍂',
+		},
+		"winter": {
+			uri: "67SvJfEofXhdIXwsuUJbCJ",
+			title: 'Winter',
+			description: 'All songs from my winter playlists',
+			emoji: '❄️',
+		},
+		"chill": {
+			uri: "0x0ZQcmLsamIx5bF5px2Tc",
+			title: 'Sloth Mode',
+			description: 'Songs to Relax to',
+			emoji: '🦥',
+		},
+		"sleep": {
+			uri: "13RQxSzWqVg3fHlalxlyhC",
+			title: 'Milk of the Poppy',
+			description: 'Songs to fall asleep to',
+			emoji: '🛌',
+		},
+		"coding": {
+			uri: "0camicGxGNwB9YS66nAwtb",
+			title: '1337 h4xX0r',
+			emoji: '🖥  ⌨️🖱',
+			description: 'Songs to code to',
+		},
+		"sports": {
+			uri: "4X1uZRUIccnwcofS8QjBku",
+			title: '',
+			description: 'Songs to run to',
+			emoji: '🏃‍➡️',
+		},
+		"meme": {
+			uri: "4HvN65IMe9oEWqMl1zHCIE",
+			title: 'Memeology',
+			description: 'Songs that became a meme',
+			emoji: '🐸',
+		},
+		"party": {
+			uri: "1E5n3EWqT9MDPUb1qvGjsb",
+			title: 'Partytime',
+			description: 'Songs to party to',
+			emoji: '🎉',
+		},
+		"night": {
+			uri: "2OHb3F8gQqQY4hb14p2uoe",
+			title: 'Latenite',
+			description: 'Songs to get through a long night',
+			emoji: '🌃',
+		},
+		"morning": {
+			uri: "6FhnyDVlq5Az6Bllb7einR",
+			title: 'the morning after',
+			description: 'Songs to cure your hangover',
+			emoji: '🌅',
+		},
+		"piano": {
+			uri: "5w86GPXC86kKEpbLSKVxFS",
+			title: 'Piano',
+			description: 'Songs I can play on the piano',
+			emoji: '🎹',
+		},
+		"clean": {
+			uri: "5idv5XxAwr7q9u4hyWNaNH",
+			title: 'Blitzblank',
+			description: 'Songs to clean your house to',
+			emoji: '🧹',
+		},
+		"cook": {
+			uri: "66wQsRoITiTV5xeG6T19wH",
+			title: 'Cooking Inspo',
+			description: 'Songs to get your taste buds ready',
+			emoji: '🧑‍🍳',
+		},
+	}
 </script>
 
 <svelte:head>
@@ -107,5 +201,24 @@
 				</a>
 			</Card.Root>
 		{/each}
+	</div>
+
+	<div class="mt-12">
+		<h2 class="my-4 text-2xl font-semibold">Other Playlists</h2>
+		<div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-6">
+			{#each Object.entries(otherPlaylists) as [key, otherPlaylist]}
+				<Card.Root>
+					<a href={SPOTIFY_PLAYLIST_LINK + otherPlaylist.uri} target="_blank">
+						<Card.Content class="pt-6">
+							<h2 class="py-2 text-xl font-semibold">
+								{otherPlaylist.title || key}
+								{otherPlaylist.emoji}
+							</h2>
+							<p class="text-muted-foreground">{otherPlaylist.description}</p>
+						</Card.Content>
+					</a>
+				</Card.Root>
+			{/each}
+		</div>
 	</div>
 </div>
