84019e274b3ff997bdfee0a6dd26156894a0fea0
Dennis Muensterer <dm@hpm.agency>
2025-01-16 01:13:29 +0100

    Add redirect to current playlist

---
+++ src/routes/playlists/current/+page.ts
@@ -0,0 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+import playlists from '../playlists.json';
+
+export function load() {
+    console.log('current', !playlists)
+    if (playlists.length > 0) {
+        const firstPlaylist = playlists[0];
+        console.log(firstPlaylist)
+        throw redirect(302, `https://open.spotify.com/playlist/${firstPlaylist.uri}`);
+    } else {
+        throw redirect(302, '/playlists');
+    }
+}
