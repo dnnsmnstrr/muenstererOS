348ae889581cc3ff71dbfe8437a0ed7a7037b124
Dennis Muensterer <dm@hpm.agency>
2025-02-09 04:09:50 +0100

    Fix settings error

--- src/routes/settings/SettingsForm.svelte
+++ src/routes/settings/SettingsForm.svelte
@@ -20,9 +20,9 @@
 	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
 	import { zodClient } from 'sveltekit-superforms/adapters';
 
-  let { data }: { data: { form: SuperValidated<Infer<SettingsSchema>> } } =
+  let { data }: { data: { form?: SuperValidated<Infer<SettingsSchema>> } } =
     $props();
-	const form = superForm(data.form, {
+	const form = superForm(data?.form || { debug: false, mode: 'system'}, {
 		validators: zodClient(settingsSchema)
 	});
 	function handleModeChange(value: string) {
@@ -40,36 +40,38 @@
 <form>
 	<Form.Field {form} name="mode">
 		<Form.Control>
-			<Form.Label>Theme Mode</Form.Label>
-			<RadioGroup.Root class="flex flex-col space-y-1" value={$mode} onValueChange={handleModeChange}>
-				<div class="flex items-center space-x-3 space-y-0">
-					<RadioGroup.Item value="light" id="light" />
-					<Label for="light" class="font-normal">Light</Label>
-				</div>
-				<div class="flex items-center space-x-3 space-y-0">
-					<RadioGroup.Item value="dark" id="dark" />
-					<Label for="dark" class="font-normal">Dark</Label>
-				</div>
-				<div class="flex items-center space-x-3 space-y-0">
-					<RadioGroup.Item value="" id="system" />
-					<Label for="system" class="font-normal">System</Label>
-				</div>
-			</RadioGroup.Root>
+        <h2>Theme Mode</h2>
+        <RadioGroup.Root class="flex flex-col space-y-1" value={$mode} onValueChange={handleModeChange}>
+          <div class="flex items-center space-x-3 space-y-0">
+            <RadioGroup.Item value="light" id="light" />
+            <Form.Label for="light" class="font-normal">Light</Form.Label>
+          </div>
+          <div class="flex items-center space-x-3 space-y-0">
+            <RadioGroup.Item value="dark" id="dark" />
+            <Form.Label for="dark" class="font-normal">Dark</Form.Label>
+          </div>
+          <div class="flex items-center space-x-3 space-y-0">
+            <RadioGroup.Item value="" id="system" />
+            <Form.Label for="system" class="font-normal">System</Form.Label>
+          </div>
+        </RadioGroup.Root>
 		</Form.Control>
 	</Form.Field>
-	<Separator />
+	<Separator class="my-6"/>
 	<Form.Field {form} name="debug">
-		<div class="flex flex-row items-start space-x-3 space-y-0 pt-6">
-			<Checkbox checked={$debug} onCheckedChange={(value) => ($debug = !!value)} />
-			<div class="space-y-1 leading-none">
-				<Form.Label class="-mt-0.5 flex items-center gap-2">
-					<span class="pb-1.5">Debug mode</span>
-					<AnimatedToggle visible={$debug}>
-						<Bug class="w-4" />
-					</AnimatedToggle>
-				</Form.Label>
-				<Form.Description>This will cause more verbose logging in the console.</Form.Description>
-			</div>
-		</div>
+    <Form.Control>
+      <div class="flex flex-row items-start space-x-3 space-y-0">
+        <Checkbox checked={$debug} onCheckedChange={(value) => ($debug = !!value)} id="debug-checkbox"/>
+        <div class="space-y-1 leading-none">
+          <Form.Label class="-mt-0.5 flex items-center gap-2" for="debug-checkbox">
+            <span class="pb-1.5">Debug mode</span>
+            <AnimatedToggle visible={$debug}>
+              <Bug class="w-4" />
+            </AnimatedToggle>
+          </Form.Label>
+          <Form.Description>This will cause more verbose logging in the console.</Form.Description>
+        </div>
+      </div>
+    </Form.Control>
 	</Form.Field>
 </form>
