9cf8eef720b83d67a02400ce4f49e535b6b01ec5
Dennis Muensterer <dm@hpm.agency>
2025-02-15 12:57:10 +0100

    Improve settings layout

--- src/routes/settings/SettingsForm.svelte
+++ src/routes/settings/SettingsForm.svelte
@@ -41,50 +41,53 @@
 </script>
 
 <form>
-	<Form.Field {form} name="mode">
+	<Form.Field {form} name="mode" class="flex justify-between">
 		<Form.Control>
-        <h2>Theme Mode</h2>
-        <RadioGroup.Root class="flex flex-col space-y-1" value={$mode} onValueChange={handleModeChange}>
-          <div class="flex items-center space-x-3 space-y-0">
-            <RadioGroup.Item value="light" id="light" />
-            <Form.Label for="light" class="font-normal">Light</Form.Label>
-          </div>
-          <div class="flex items-center space-x-3 space-y-0">
-            <RadioGroup.Item value="dark" id="dark" />
-            <Form.Label for="dark" class="font-normal">Dark</Form.Label>
-          </div>
-          <div class="flex items-center space-x-3 space-y-0">
-            <RadioGroup.Item value="" id="system" />
-            <Form.Label for="system" class="font-normal">System</Form.Label>
-          </div>
-        </RadioGroup.Root>
+			<div class="flex flex-col space-y-2">
+				<h2>Mode</h2>
+				<RadioGroup.Root class="flex flex-col space-y-1" value={$mode} onValueChange={handleModeChange}>
+				  <div class="flex items-center space-x-3 space-y-0">
+					<RadioGroup.Item value="light" id="light" />
+					<Form.Label for="light" class="font-normal">Light</Form.Label>
+				  </div>
+				  <div class="flex items-center space-x-3 space-y-0">
+					<RadioGroup.Item value="dark" id="dark" />
+					<Form.Label for="dark" class="font-normal">Dark</Form.Label>
+				  </div>
+				  <div class="flex items-center space-x-3 space-y-0">
+					<RadioGroup.Item value="" id="system" />
+					<Form.Label for="system" class="font-normal">System</Form.Label>
+				  </div>
+				</RadioGroup.Root>
+			</div>
 		</Form.Control>
-		
+		<div class="grid grid-cols-3 gap-2">
+			<h2 class="col-span-3">Theme</h2>
+			{#each themes as theme (theme.name)}
+				{@const isActive = $themeStore === theme.name}
+				<Button
+					variant="outline"
+					size="sm"
+					onclick={() => {
+						$themeStore = theme.name;
+					}}
+					class={cn("justify-start", isActive && "border-primary border-2")}
+					style="--theme-primary: hsl({theme.activeColor[$mode ?? 'dark']})"
+				>
+					<span
+						class="mr-1 flex h-5 w-5 shrink-0 -translate-x-1 items-center justify-center rounded-full bg-[--theme-primary]"
+					>
+						{#if isActive}
+							<Check class="h-4 w-4 text-white" />
+						{/if}
+					</span>
+					{theme.label}
+				</Button>
+			{/each}
+		</div>
 	</Form.Field>
 	<Separator class="my-6"/>
-	<div class="grid grid-cols-3 gap-2">
-		{#each themes as theme (theme.name)}
-			{@const isActive = $themeStore === theme.name}
-			<Button
-				variant="outline"
-				size="sm"
-				onclick={() => {
-					$themeStore = theme.name;
-				}}
-				class={cn("justify-start", isActive && "border-primary border-2")}
-				style="--theme-primary: hsl({theme.activeColor[$mode ?? 'dark']})"
-			>
-				<span
-					class="mr-1 flex h-5 w-5 shrink-0 -translate-x-1 items-center justify-center rounded-full bg-[--theme-primary]"
-				>
-					{#if isActive}
-						<Check class="h-4 w-4 text-white" />
-					{/if}
-				</span>
-				{theme.label}
-			</Button>
-		{/each}
-	</div>
+	
 	<Form.Field {form} name="debug">
     <Form.Control>
       <div class="flex flex-row items-start space-x-3 space-y-0">
