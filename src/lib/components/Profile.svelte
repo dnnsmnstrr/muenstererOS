4cf26e0c6eeb8d7eeca43ad560eab029e47efebd
Dennis Muensterer <dennismuensterer@gmail.com>
2025-02-16 15:54:47 +0100

    Use avatar service for profile image

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -7,9 +7,9 @@
 
 </script>
 
-  <Card.Header class="flex flex-row items-center justify-start gap-6 flex-wrap mt-6">
+  <Card.Header class="flex flex-row items-center justify-start gap-6 flex-wrap mt-4 md:mt-0">
     <Avatar.Root class="h-24 w-24">
-      <Avatar.Image src="https://github.com/dnnsmnstrr.png" alt="@dnnsmnstrr" />
+      <Avatar.Image src="/.well-known/avatar?github" alt="@dnnsmnstrr" />
       <Avatar.Fallback>{NAME_ABBREVIATION}</Avatar.Fallback>
     </Avatar.Root>
     <div class="flex flex-col gap-2">
@@ -25,8 +25,8 @@
           {/each}
         </Typewriter>
       </Card.Description>
-      <Card.Footer class="p-0 pt-2">
-        <Button href={links.cv} size="sm">Find out more...</Button>
+      <Card.Footer class="hidden md:block p-0 pt-2">
+        <Button href={links.cv} size="sm" class="">Find out more...</Button>
       </Card.Footer>
     </div>
   </Card.Header>
