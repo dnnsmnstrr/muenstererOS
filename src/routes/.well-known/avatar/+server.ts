1bf455b1ea3374cedc0f0f64015f83a659733eae
Dennis Muensterer <dennismuensterer@gmail.com>
2025-02-16 13:47:11 +0100

    Add avatar endpoint

---
+++ src/routes/.well-known/avatar/+server.ts
@@ -0,0 +1,36 @@
+import { redirect, type RequestHandler } from '@sveltejs/kit';
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+import {
+    EMAIL
+} from '../../../lib/config'
+
+const DEFAULT_SIZE = 200;
+function createGravatarUrl(email: string, size: number = DEFAULT_SIZE) {
+    const hash = crypto.createHash('sha256').update(email).digest('hex');
+    return `https://www.gravatar.com/avatar/${hash}?s=${size}`;
+}
+
+export const GET: RequestHandler = async ({ url }) => {
+    let modifier = '';
+    let fileType = 'jpeg';
+
+    if (url.searchParams.has('gravatar')) {
+        const gravatarUrl = createGravatarUrl(EMAIL, url.searchParams.get('size') ? parseInt(url.searchParams.get('size')!) : DEFAULT_SIZE);
+        throw redirect(302, gravatarUrl);
+    }
+
+    if (url.search === '?square') {
+        modifier = '-square';
+    }
+    const imagePath = path.resolve(`static/avatar${modifier}.${fileType}`);
+    const imageBuffer = fs.readFileSync(imagePath);
+
+    return new Response(imageBuffer, {
+        status: 200,
+        headers: {
+            'Content-Type': 'image/' + fileType,
+        }
+    });
+};
