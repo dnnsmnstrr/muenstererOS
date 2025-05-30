1209a9c6d7e784959000ce30f90e4ee98d79dcd9
Dennis Muensterer <dennismuensterer@gmail.com>
2025-05-30 14:13:46 +0200

    Add links to changelog

--- src/routes/log/+page.svelte
+++ src/routes/log/+page.svelte
@@ -6,6 +6,7 @@
 		date: string;
 		title: string;
 		description: string;
+		path?: string;
 	};
 	const changelog = JSON.parse(changes) as ChangeLogEntry[];
 </script>
@@ -20,7 +21,7 @@
 		{#each changelog as entry, i}
 			<li class="mb-10 ml-4 -translate-y-4 last:mb-0">
 				<div class="absolute w-3 h-3 bg-primary rounded-full -translate-x-[22px] translate-y-2"></div>
-				<h2 class="text-lg font-bold">{entry.title}</h2>
+				<h2 class="text-lg font-bold"><a href="{entry.path}">{entry.title}</a></h2>
 				<p class="text-sm text-gray-500">{entry.date}</p>
 				<p>{entry.description}</p>
 			</li>
