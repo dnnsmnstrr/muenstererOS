82c02eef24112f0deeb84ff0b5181b705f8f23d2
Dennis Muensterer <dennismuensterer@gmail.com>
2025-06-11 23:09:55 +0200

    Add h-card to footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -4,8 +4,8 @@
   const currentYear = new Date().getFullYear();
 </script>
 
-<footer class="pb-1 pr-1 flex justify-center gap-2 [&>*]:opacity-30">
-  <p class="flex gap-2">© {currentYear} <span class="hidden sm:block">{OWNER_NAME}</span></p>
+<footer class="pb-1 pr-1 flex justify-center gap-2 [&>*]:opacity-30 h-card">
+  <p class="flex gap-2">© {currentYear} <a class="hidden sm:block p-name" href="https://muensterer.tech/" rel="me">{OWNER_NAME}</a></p>
   <span>•</span>
   <a href="/legal" class="hover:opacity-70 transition-opacity flex gap-1">Legal Notice & Privacy Policy</a>
 </footer>
