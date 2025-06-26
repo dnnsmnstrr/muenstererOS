71528c913d5b55c3ff0de9da1d3d829e3ca9c24a
Dennis Muensterer <dennismuensterer@gmail.com>
2025-06-26 21:16:02 +0200

    Create pages list to generate sitemap from

--- scripts/xml.js
+++ scripts/xml.js
@@ -4,69 +4,24 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// Use absolute path to static/changes.json relative to project root
-const changes = JSON.parse(fs.readFileSync(path.join(__dirname, '../static/changes.json'), 'utf-8'));
+
+const pages = JSON.parse(fs.readFileSync(path.join(__dirname, '../static/pages.json'), 'utf-8')); // { title, path, date }
+const changes = JSON.parse(fs.readFileSync(path.join(__dirname, '../static/changes.json'), 'utf-8')); // { name, description, path, date }
 
 const baseUrl = 'https://muensterer.tech'; 
 
-const routes = [
-    '/',
-    '/about',
-    '/contact',
-    '/settings',
-    '/legal',
-    '// pages',
-    '/uses',
-    '/playlists',
-    '/projects',
-];
-
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${routes.map(route => route.startsWith('//') ? 
-        `
-    <!-- ${route.replace('//', '')} -->`
-    : `
-    <url>
-        <loc>${baseUrl}${route}</loc>
-    </url>`).join('')}
+    ${pages.map(page => `
+        <url>
+            <loc>${baseUrl}${page.path}</loc>
+            <lastmod>${page.date}</lastmod>
+        </url>
+    `).join('')}
 </urlset>`;
 
 fs.writeFileSync(path.join(__dirname, '..', 'static', 'sitemap.xml'), sitemap);
-console.log('Sitemap generated!');
-
-const pages = [
-    {
-        name: 'About',
-        content: 'The about page',
-        path: '/about',
-        date: '2025-01-01',
-    },
-    {
-        name: 'Contact',
-        content: 'How to contact me',
-        path: '/contact',
-        date: '2025-01-01',
-    },
-    {
-        name: 'Uses',
-        content: 'The tools I use',
-        path: '/uses',
-        date: '2025-02-01',
-    },
-    {
-        name: 'Playlists',
-        content: 'My playlists',
-        path: '/playlists',
-        date: '2025-01-31',
-    },
-    {
-        name: 'Where',
-        content: 'Where I am',
-        path: '/where',
-        date: '2025-02-20'
-    }
-]
+console.log(`Sitemap generated with ${pages.length} pages!`);
 
 const name = 'Dennis Muensterer';
 const idCounter = {
@@ -90,7 +45,7 @@ function generateFeedItem(item) {
         </content>
     </entry>`;
 }
-const pagesFeed = pages.reverse().map(generateFeedItem).reverse().join('');
+
 const changesFeed = changes.reverse().map(generateFeedItem).reverse().join('');
 const feedAtom = `<?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
@@ -102,7 +57,8 @@ const feedAtom = `<?xml version="1.0" encoding="utf-8"?>
     <author>
         <name>${name}</name>
     </author>
-    ${changesFeed}${pagesFeed}
+    ${changesFeed}
 </feed>`;
 fs.writeFileSync(path.join(__dirname, '..', 'static', 'feed.xml'), feedAtom);
-console.log('Feed generated!');
+
+console.log('Atom Feed generated!');
