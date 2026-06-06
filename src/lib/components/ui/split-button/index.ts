ca53ddbd34d8a9862d4eda72f786ca514e307bca
Dennis Muensterer <dennismuensterer@gmail.com>
2026-06-06 20:29:34 +0200

    build: configure jsrepo with shadcn svelte extras

--- src/lib/components/ui/split-button/index.ts
+++ src/lib/components/ui/split-button/index.ts
@@ -1,9 +1,9 @@
-import Root from './split-button.svelte';
-import Action from './split-button-action.svelte';
-import Select from './split-button-select.svelte';
-import SelectTrigger from './split-button-select-trigger.svelte';
-import SelectAction from './split-button-select-action.svelte';
-import SelectContent from './split-button-content.svelte';
+import Root from '$lib/components/ui/split-button/split-button.svelte';
+import Action from '$lib/components/ui/split-button/split-button-action.svelte';
+import Select from '$lib/components/ui/split-button/split-button-select.svelte';
+import SelectTrigger from '$lib/components/ui/split-button/split-button-select-trigger.svelte';
+import SelectAction from '$lib/components/ui/split-button/split-button-select-action.svelte';
+import SelectContent from '$lib/components/ui/split-button/split-button-content.svelte';
 // re-export select components
 import {
 	SelectGroup,
@@ -11,12 +11,12 @@ import {
 	SelectSeparator
 } from '$lib/components/ui/select';
 
-export type { SplitButtonProps, SplitButtonPropsWithoutHTML } from './split-button.svelte';
-export type { SplitButtonActionProps } from './split-button-action.svelte';
-export type { SplitButtonSelectProps } from './split-button-select.svelte';
-export type { SplitButtonSelectTriggerProps } from './split-button-select-trigger.svelte';
-export type { SplitButtonSelectActionProps } from './split-button-select-action.svelte';
-export type { SplitButtonClickEvent } from './split-button.svelte.ts';
+export type { SplitButtonProps, SplitButtonPropsWithoutHTML } from '$lib/components/ui/split-button/split-button.svelte';
+export type { SplitButtonActionProps } from '$lib/components/ui/split-button/split-button-action.svelte';
+export type { SplitButtonSelectProps } from '$lib/components/ui/split-button/split-button-select.svelte';
+export type { SplitButtonSelectTriggerProps } from '$lib/components/ui/split-button/split-button-select-trigger.svelte';
+export type { SplitButtonSelectActionProps } from '$lib/components/ui/split-button/split-button-select-action.svelte';
+export type { SplitButtonClickEvent } from '$lib/components/ui/split-button/split-button.svelte.js';
 
 export {
 	Root,
